2a0ad5e3b231943fe838bee9fb90a0f4c84d369d
AmiraWalid1 <amirawaleed012@gmail.com>
2023-07-15 16:42:47 -0700

    print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 99 ; i++)
+	{
+		for (int j = i + 1 ; j <= 99 ; j++)
+		{
+			putchar(((i % 100) - (i % 10)) / 10 + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar(((j % 100) - (j % 10)) / 10 + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
