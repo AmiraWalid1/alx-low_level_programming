8f5d801e8eb9a702d398d437b13e85501f5f894d
AmiraWalid1 <amirawaleed012@gmail.com>
2023-07-18 18:28:33 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 		{
 			int res = i * j;
 
-			if (res > 10)
+			if (res >= 10)
 				_putchar(res / 10 + '0');
-			else 
+			else if (res < 10 && j != 0)
 				_putchar(' ');
 			_putchar(res % 10 + '0');
 			if (j == 9)
