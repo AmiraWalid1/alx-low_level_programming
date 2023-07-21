d11eb86159424d2b6c87a527f63189a8e596729d
AmiraWalid1 <amirawaleed012@gmail.com>
2023-07-20 18:02:06 -0700

    print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,14 @@
  */
 void print_number(int n)
 {
+	unsinged int num = n;
+
 	if (n < 0)
 	{
-		n *= -1;
+		num = -num;
 		_putchar('-');
 	}
-	if (n / 10 > 0)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
 }
