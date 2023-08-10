e8b76727c81442ef73dd3c43eeab08f03499fbe2
Amira Walid <amirawaleed012@gmail.com>
2023-08-10 16:16:08 +0300

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * isnum - check if str is num or not
+ * @str: string to check it
+ *
+ * Return: 1 (true) / 0 (false)
+ */
+int isnum(char const *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/**
+ * main - Entry point.
+ *
+ * Description: multiplies two positive numbers.
+ * @argc: num of argument
+ * @argv: array contain value of argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3 || !isnum(argv[1]) || !isnum(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
