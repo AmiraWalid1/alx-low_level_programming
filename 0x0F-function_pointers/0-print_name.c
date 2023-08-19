0addee0fffa3215897f76f51d8b9fc14bc339ffd
Amira Walid <amirawaleed012@gmail.com>
2023-08-19 13:18:27 +0300

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - print name using function pointer f
  * @name: name that will print
  * @f: function pointer
- * 
+ *
  * Return: void
 */
 void print_name(char *name, void (*f)(char *))
