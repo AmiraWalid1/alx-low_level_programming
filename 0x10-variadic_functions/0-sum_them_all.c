d3638baec5fe499345d5acd88b5dcead58262355
Amira Walid <amirawaleed012@gmail.com>
2023-08-19 20:27:02 +0300

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,20 +4,20 @@
 /**
  * sum_them_all - sum of all its parameters.
  * @n: num of argument
- * 
+ *
  * Return: sum of all its parameters.
 */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int sum = 0, i;
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
 	va_start(nums, n);
-	for(i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		sum += va_arg(nums, int);
 	}
