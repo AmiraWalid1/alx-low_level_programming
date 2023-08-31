d0f9484d487067c353eed13f362b7462a91443d1
AmiraWalid1 <amirawaleed012@gmail.com>
2023-09-01 01:30:04 +0300

    set bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ void print_binary(unsigned long int n);
 void _putchar(char c);
 unsigned long int _log2(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /*MAIN_H*/
