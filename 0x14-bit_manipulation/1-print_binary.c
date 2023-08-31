7cd40a82bca9630fc5b14fcf4b3636386c71d3ab
AmiraWalid1 <amirawaleed012@gmail.com>
2023-09-01 00:58:31 +0300

    fix

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _log2 - log2 of number
+ * @n: number
+ *
+ * Return: log2(n)
+*/
+unsigned long int _log2(unsigned long int n)
+{
+	unsigned long int res = -1;
+
+	while (n)
+	{
+		n >>= 1;
+		res++;
+	}
+	return (res);
+}
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number.
+ *
+ * Return: void.
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = _log2(n) + 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (i--)
+	{
+		if (n & (1 << i))
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
