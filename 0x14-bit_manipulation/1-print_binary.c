4a5224b28f4874d4ea6da6fb00c05144a14ae9e2
AmiraWalid1 <amirawaleed012@gmail.com>
2023-08-31 15:37:51 +0300

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _log2 - log2 of x
+ * @x: num
+ *
+ * Return: log2(x)
+*/
+int _log2(int x)
+{
+	int res = 0;
+
+	while (x)
+	{
+		x /= 2;
+		res++;
+	}
+	return (res);
+}
+/**
  * print_binary - prints the binary representation of a number.
  * @n: number.
  *
@@ -8,21 +25,15 @@
 */
 void print_binary(unsigned long int n)
 {
-	int binary_num[32], i = 0;
+	unsigned long int i = _log2(n);
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while (n)
+	while (i--)
 	{
-		binary_num[i] = (n % 2);
-		n /= 2;
-		i++;
-	}
-	for (i = i - 1 ; i >= 0 ; i--)
-	{
-		_putchar(binary_num[i] + '0');
+		_putchar((n & (1 << i)) ? '1' : '0');
 	}
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,5 @@ unsigned int binary_to_uint(const char *b);
 int _pow(int base, int power);
 void print_binary(unsigned long int n);
 void _putchar(char c);
+int _log2(int x);
 #endif /*MAIN_H*/
