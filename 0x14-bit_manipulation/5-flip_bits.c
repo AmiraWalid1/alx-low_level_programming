6ceff84f21e5cefc215f862839106919fcb9bb30
AmiraWalid1 <amirawaleed012@gmail.com>
2023-09-01 02:02:55 +0300

    fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,5 @@
 #include "main.h"
-/**
- * _log2 - log2 of number
- * @n: number
- *
- * Return: log2(n)
-*/
-unsigned long int _log2(unsigned long int n)
-{
-	unsigned long int res = -1;
 
-	while (n)
-	{
-		n >>= 1;
-		res++;
-	}
-	return (res);
-}
 /**
  * flip_bits - count the number of bits you would need to flip to get
  * from one number to another.
@@ -36,7 +20,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		{
 			cnt++;
 		}
-		nXORm >>= 1ul ;
+		nXORm >>= 1ul;
 	}
 	return (cnt);
 
