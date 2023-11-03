3f7ac84ad37f46df3d0e03719d36f924d61c9974
Amira Walid <amirawaleed012@gmail.com>
2023-11-03 18:24:54 +0200

    hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,22 +11,59 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int idx_key;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *curr;
 
 	if (key == NULL || strlen(key) == 0)
 		return (0);
 	idx_key = key_index((const unsigned char *)key, ht->size);
+	curr = ht->array[0];
+	while (curr)
+	{
+		if (strcmp(curr->value, (char *) value) == 0)
+		{
+			free(curr->value);
+			curr->value =  _strdup((char *)value);
+			if (curr->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		curr = curr->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
-	new_node->value = malloc((strlen(value) + 1) * sizeof(char));
-	if (new_node->value == NULL)
+	new_node->key = _strdup((char  *)key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
 		return (0);
-	strcpy(new_node->value, value);
-
+	}
+	new_node->value = _strdup((char *)value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[idx_key];
 	ht->array[idx_key] = new_node;
-
 	return (1);
+}
+/**
+ * _strdup - function returns a pointer to a new string
+ * which is a duplicate of the string str.
+ * @str: string will be duplicated
+ * Return: pointer to a new string which is a duplicate of the string str.
+*/
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	new_str = malloc((strlen(str) + 1) * sizeof(str));
+	if (new_str == NULL)
+		return (NULL);
+	strcpy(new_str, str);
+	return (new_str);
 }
